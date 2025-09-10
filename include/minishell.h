faddaae737da41ef0c7d2583ce3a1993494cafa0
Moritz Diepgen <modiepge@3-G-4.42heilbronn.de>
2025-09-10 15:29:47 +0200

    feat: added garbage collection via bookkeeping
    feat: added function to access static "data"-struct
    docs: added "Shell Command Language.pdf" and "Gnu bash manual.pdf" to
    docs. garbage collector usage is not yet documented.

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: modiepge <modiepge@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 20:59:40 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/09/05 11:03:27 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/09/10 15:28:23 by modiepge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,9 +38,81 @@
 # include <limits.h>	// INT_MAX, PATH_MAX (is not a function)
 
 # include "libft.h"
+# include "libft_printf.h"
 
+typedef enum	e_token_type
+{
+	TOK_COMMAND,
+	TOK_WHITESPACE,
+	TOK_ARGUMENT,
+	TOK_LEFT_PARENTHESIS,
+	TOK_RIGHT_PARENTHESIS,
+	TOK_LESS,
+	TOK_DOUBLE_LESS,
+	TOK_GREATER,
+	TOK_DOUBLE_GREATER,
+	TOK_QUOTE,
+	TOK_DOUBLE_QUOTE,
+	TOK_AND,
+	TOK_OR,
+	TOK_VARIABLE,
+	TOK_WILDCARD
+}	t_token_type;
+
+typedef	enum	e_gc_index
+{
+	GC_LEXING,
+	GC_EXPANSION,
+	GC_PARSING,
+	GC_EXECUTION,
+	GC_PERSISTENT
+}	t_gc_index;
+
+typedef union	u_gc_book
+{
+	struct
+	{
+		t_list	*lexing;
+		t_list	*expansion;
+		t_list	*parsing;
+		t_list	*execution;
+		t_list	*persistent;
+	};
+	t_list	*lists[5];
+}	t_gc_book;
+
+typedef struct	s_token
+{
+	t_token_type	type;
+	char			*content;
+	struct s_token	*next;
+	struct s_token	*prev;
+}	t_token;
+
+typedef struct	s_data
+{
+	char**		envp;
+	char*		line;
+	t_token		*tokens;
+	t_gc_index	gc_mode;
+	t_gc_book	gc_book;
+}	t_data;
 
 // auto
+t_list		*gc_add(void *memory);
+void		gc_clear_all(void);
+void		gc_mode(t_gc_index mode);
+void		gc_clear(t_gc_index index);
+char		*gc_substr(char const *string, unsigned int start, size_t length);
+char		*gc_strdup(char const *string);
+void		*gc_calloc(size_t count, size_t size);
+t_data		*data(void);
 int			main(int argc, char **argv, char **envp);
+int			is_reserved(char *position);
+void		token_whitespace(char *string);
+t_token		*token_new(t_token_type type, char *content);
+t_token		*token_last(t_token	*list);
+t_token		*token_add(t_token *token);
+void		token_delete(t_token *token);
 
 #endif
