826fd30ebdebdd713691ae8fdf341e424ee4afe4
Moritz Diepgen <modiepge@3-G-4.42heilbronn.de>
2025-09-10 15:29:47 +0200

    feat: added garbage collection via bookkeeping
    feat: added function to access static "data"-struct
    docs: added "Shell Command Language.pdf" and "Gnu bash manual.pdf" to
    docs. garbage collector usage is not yet documented.

---
+++ src/garbage_collector.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   garbage_collector.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: modiepge <modiepge@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 14:32:15 by modiepge          #+#    #+#             */
+/*   Updated: 2025/09/10 15:28:29 by modiepge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	gc_error(void * memory, char *message)
+{
+	if (memory)
+		free(memory);
+	gc_clear_all();
+	ft_fprintf(2, message);
+	exit(EXIT_FAILURE);
+}
+
+static t_list	**gc_list(t_gc_index index)
+{
+	return (&data()->gc_book.lists[index]);
+}
+
+t_list	*gc_add(void *memory)
+{
+	t_list	**list;
+	t_list	*new;
+
+	if (!memory)
+		gc_error(memory, "minishell: heap allocation failed (content)");
+	new = ft_lstnew(memory);
+	if (!new)
+		gc_error(memory, "minishell: heap allocation failed (bookkeeping)");
+	list = gc_list(data()->gc_mode);
+	if (list)
+		ft_lstadd_front(list, new);
+	else
+		*list = new;
+	return (*list);
+}
+
+void	gc_mode(t_gc_index mode)
+{
+	t_gc_index	*index;
+
+	index = 0;
+	*index = data()->gc_mode;
+	*index = mode;
+}
+
+void	gc_clear(t_gc_index index)
+{
+	t_list	*current;
+	t_list	*delete;
+	t_list	**list;
+
+	list = gc_list(index);
+	if (!list)
+		return ;
+	current = *list;
+	delete = NULL;
+	while (current)
+	{
+		delete = current;
+		current = current->next;
+		free(delete->content);
+		free(delete);
+	}
+	*list = NULL;
+}
+
+void	gc_clear_all(void)
+{
+	t_gc_index	index;
+
+	index = 0;
+	while (index <= GC_PERSISTENT)
+		gc_clear(index++);
+}
+
+char	*gc_substr(char const *string, unsigned int start, size_t length)
+{
+	return (gc_add(ft_substr(string, start, length))->content);
+}
+
+char	*gc_strdup(char const *string)
+{
+	return (gc_add(ft_strdup(string))->content);
+}
+
+void	*gc_calloc(size_t count, size_t size)
+{
+	void	*add;
+
+	add = ft_calloc(count, size);
+	gc_add(add);
+	return (add);
+}
--- src/main.c
+++ src/main.c
@@ -3,15 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: modiepge <modiepge@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 20:58:57 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/09/05 13:23:39 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/09/10 14:41:06 by modiepge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
+t_data	*data(void)
+{
+	static t_data	data;
+
+	return (&data);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
