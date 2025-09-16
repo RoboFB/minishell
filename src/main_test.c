1e1e7170b6fb537367ad60a0414a0b1cbe6cf04f
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-09-16 18:20:14 +0200

    feat: tests for env with a main function

--- src/main_test.c
+++ src/main_test.c
@@ -6,7 +6,7 @@
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 20:58:57 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/09/12 22:01:48 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/09/16 18:17:24 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,15 +59,51 @@ int main(int argc, char **argv, char **envp)
 			printf("test: env not correct copied:\nnew:%s\nold:%s\n", env_new[i], envp[i]);
 		i++;
 	}
+	printf("PATH: %s\n", *env_get_line_ptr("PATH"));
+	env_get_line_ptr("PATHgrerg") == NULL ? printf("OK ") : printf("\nERROR 1\n");
+	env_get_line_ptr("wPATH") == NULL ? printf("OK ") : printf("\nERROR 2\n");
+	env_get_line_ptr("path") == NULL ? printf("OK ") : printf("\nERROR 3\n");
+	env_get_line_ptr("=") == NULL ? printf("OK ") : printf("\nERROR 4\n");
+	env_get_line_ptr("") == NULL ? printf("OK ") : printf("\nERROR 5\n");
+	env_get_line_ptr("PH") == NULL ? printf("OK ") : printf("\nERROR 6\n");
 
-	// env_add_line("gggg=eeee");
-	// i = 0;
-	// while (envp[i] != NULL)
-	// {
-	// 	if (ft_strncmp(env_new[i], envp[i], ft_strlen(envp[i])) != 0)
-	// 		printf("test: env not correct copied:\nnew:%s\nold:%s\n", env_new[i], envp[i]);
-	// 	i++;
-	// }
+	// normal key= value
+	env_add_line("auto_test= ergerg erge -!@#$%^&*()");
+	ft_strncmp(env_get_line_data("auto_test"), " ergerg erge -!@#$%^&*()", 24) == 0
+		? printf("OK ") : printf("\nERROR 7\n");
+
+	// overwrite
+	env_add_line("auto_test=1");
+	env_get_line_data("auto_test") ? printf("OK ") : printf("\nERROR 8\n");
+
+	// no value without =
+	env_add_line("auto_test4");
+	env_get_line_data("auto_test4") == NULL ? printf("OK ") : printf("\nERROR 9\n");
+
+	// no value with =
+	env_add_line("auto_test5=");
+	*env_get_line_data("auto_test5") == '\0' ? printf("OK ") : printf("\nERROR 10\n");
+
+	env_get_line_data("no_entry") == NULL ? printf("OK ") : printf("\nERROR 11\n");
+
+	// not defined dont do that
+	env_add_line("=2");
+	env_add_line("=23523se");
+
+
+	// remove tests:
+	env_add_line("test_rm=test");
+	env_remove_line("test_rm=tes");
+	env_get_line_ptr("test_rm") == NULL ? printf("OK ") : printf("\nERROR\n");
+	env_add_line("test_rm_1");
+	env_remove_line("test_rm_1=tes");
+	env_get_line_ptr("test_rm_1") == NULL ? printf("OK ") : printf("\nERROR\n");
+
+	env_remove_line("PATH=wefew rgf");
+	env_get_line_ptr("PATH") == NULL ? printf("OK ") : printf("\nERROR\n");
+
+
+
 
 	return 0;
 }
