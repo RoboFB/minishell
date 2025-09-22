f9d87dcb9bd76695b6c10ba5f7d87ea8be6ca28d
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-09-22 15:03:30 +0200

    feat: build in echo

---
+++ src/execution/built_ins/blt_echo.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   blt_echo.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/16 19:13:15 by rgohrig           #+#    #+#             */
+/*   Updated: 2025/09/19 19:03:24 by rgohrig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	blt_echo(t_command *cmd)
+{
+	int		idx;
+	bool	newline;
+
+	newline = true;
+	idx = 0;
+	while (h_is_n_flag(cmd->args[idx]))
+	{
+		idx++;
+		newline = false;
+	}
+	while (cmd->args && cmd->args[idx])
+	{
+		ft_printf("%s", cmd->args[idx]);
+		if (cmd->args[idx + 1] != NULL)
+			ft_printf(" ");
+		idx++;
+	}
+	if (newline)
+		ft_printf("\n");
+	gc_clear_all();
+	exit(EXIT_SUCCESS);
+}
+
+bool	h_is_n_flag(char *str)
+{
+	int	idx;
+
+	idx = 0;
+	if (str[0] != '-')
+		return (false);
+	while (str[idx] == 'n')
+		idx++;
+	if (str[idx] == '\0')
+		return (true);
+	return (false);
+}
