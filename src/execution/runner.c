3dad070797d383021bbfac7d544e5e8838790da2
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-09-23 19:11:58 +0200

    fix: delead test.txt files

---
+++ src/execution/runner.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   runner.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 16:36:02 by rgohrig           #+#    #+#             */
+/*   Updated: 2025/09/22 16:41:51 by rgohrig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "minishell.h"
+
+void run_command(t_expression *cmd)
+{
+	// fork
+	// in child set redirect
+	// exe command or builtin
+	// in parent wait for child
+	(void)cmd;
+}
