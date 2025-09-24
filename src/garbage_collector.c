a4460443c0f76e53edbb53d0bb8c292b00d98fdf
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-09-24 18:35:01 +0200

    feat: gc and save wrape for get dir and change dir

--- src/garbage_collector.c
+++ src/garbage_collector.c
@@ -6,7 +6,7 @@
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 14:32:15 by modiepge          #+#    #+#             */
-/*   Updated: 2025/09/22 15:27:46 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/09/24 18:01:46 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,6 +132,16 @@ void	*gc_calloc(size_t count, size_t size)
 	return (add);
 }
 
+char *gc_getcwd(void)
+{
+	char *path;
+
+	path = getcwd(NULL, 0);
+	if (path == NULL)
+		perror("getcwd failed");
+	gc_add(path);
+	return (path);
+}
 
 // R: *new_ptr
 // sets to zero and copy ptr to new-ptr cuts if new is smaller
--- src/utils/save.c
+++ src/utils/save.c
@@ -6,7 +6,7 @@
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 20:47:03 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/09/23 18:51:55 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/09/24 17:53:11 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	save_dup2(int old_fd, int new_fd)
 	return ;
 }
 
-void	save_pipe( int *write_in_pipe, int *read_out_pipe)
+void	save_pipe(int *write_in_pipe, int *read_out_pipe)
 {
 	int 	pipe_fds[2];
 
@@ -30,5 +30,24 @@ void	save_pipe( int *write_in_pipe, int *read_out_pipe)
 		perror_exit("create_pipe failed", EXIT_FAILURE);
 	*read_out_pipe = pipe_fds[0];
 	*write_in_pipe = pipe_fds[1];
+	return ;
+}
+
+
+// mallocates buffer if buf is NULL internally without gc
+char *save_getcwd(char *buf, size_t size)
+{
+	char *path;
+
+	path = getcwd(buf, size);
+	if (path == NULL)
+		perror_exit("getcwd failed", EXIT_FAILURE);
+	return (path);
+}
+
+void save_chdir(const char *new_dir)
+{
+	if (chdir(new_dir) < 0)
+		perror_exit("chdir failed", EXIT_FAILURE);
 	return ;
 }
