d4880be4fb2304fc499353fe9d684b409dd4b75c
modiepge <modiepge@student.42heilbronn.de>
2025-10-03 19:15:29 +0200

    feat: added animated prompt, caution: also overwrites heredoc prompt

--- src/main_test.c
+++ src/main_test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_test.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: modiepge <modiepge@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 20:58:57 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/09/24 19:14:42 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/10/03 16:48:32 by modiepge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,46 @@ t_data	*data(void)
 	return (&data);
 }
 
+void	animation(void)
+{
+	const char	*frames[20] = {
+		STYLE BG_CUSTOM "196" AND BOLD START " minishel🚂" END, 
+		STYLE BG_CUSTOM "197" AND BOLD START " minishe🚂 " END, 
+		STYLE BG_CUSTOM "198" AND BOLD START " minish🚂l " END, 
+		STYLE BG_CUSTOM "199" AND BOLD START " minis🚂ll " END, 
+		STYLE BG_CUSTOM "200" AND BOLD START " mini🚂ell " END, 
+		STYLE BG_CUSTOM "201" AND BOLD START " min🚂hell " END, 
+		STYLE BG_CUSTOM "207" AND BOLD START " mi🚂shell " END, 
+		STYLE BG_CUSTOM "213" AND BOLD START " m🚂ishell " END, 
+		STYLE BG_CUSTOM "219" AND BOLD START " 🚂nishell " END, 
+		STYLE BG_CUSTOM "225" AND BOLD START "🚂inishell " END,
+		STYLE BG_CUSTOM "231" AND BOLD START " minishell " END, 
+		STYLE BG_CUSTOM "230" AND BOLD START " minishell " END, 
+		STYLE BG_CUSTOM "229" AND BOLD START " minishell " END, 
+		STYLE BG_CUSTOM "228" AND BOLD START " minishell " END, 
+		STYLE BG_CUSTOM "227" AND BOLD START " minishell " END, 
+		STYLE BG_CUSTOM "226" AND BOLD START " minishell " END, 
+		STYLE BG_CUSTOM "220" AND BOLD START " minishell " END, 
+		STYLE BG_CUSTOM "214" AND BOLD START " minishell " END, 
+		STYLE BG_CUSTOM "208" AND BOLD START " minishell " END, 
+		STYLE BG_CUSTOM "202" AND BOLD START " minishell " END
+	};
+	const t_expression sleep = {OPERATOR_CMD, NULL, NULL, NULL, "sleep", (char *[]){"sleep", ".09", NULL}, NULL, (t_tokens){NULL, NULL, 0}};
+	static int	t;
+	int			pid;
+
+	if (t >= 20)
+		t = 0;
+	ft_printf("\033[s\r%s %% \033[u", frames[t]);
+	pid = exe_command((t_expression *)&sleep);
+	wait_and_set_exit_code(pid);
+	t++;
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	char *line;
+	char	*line;
+	int		pid;
 	
 	(void)argc;
 	(void)argv;
@@ -31,15 +68,23 @@ int	main(int argc, char **argv, char **envp)
 	gc_init();
 	gc_mode(GC_PERSISTENT);
 	env_init(envp);
+	pid = save_fork();
 	while (1)
 	{
-		line = readline(STYLE BG_WHITE AND BOLD START " minishell " END " % ");
-		tokenize(line);
-		if (line && line[0] != '\0')
+		if (!pid)
+		{
+			animation();
+		}
+		else
+		{
+			line = readline("\r minishell  % ");
+			tokenize(line);
+			if (line && line[0] != '\0')
 			add_history(line);
-		run_all();
-		gc_clear_temporary();
-		free(line);
+			run_all();
+			gc_clear_temporary();
+			free(line);
+		}
 	}
 	gc_clear_all();
 	return (0);
