696ea80ef72ab79e9bb3f07608c7cacfa88fc299
modiepge <modiepge@student.42heilbronn.de>
2025-10-09 03:31:58 +0200

    feat: collecting bytes for heredocs on delimiter tokens for later expansion and writing to file

--- src/parsing/processing/parse_redirects.c
+++ src/parsing/processing/parse_redirects.c
@@ -3,57 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   parse_redirects.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: modiepge <modiepge@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 18:07:13 by modiepge          #+#    #+#             */
-/*   Updated: 2025/10/08 17:04:38 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/10/09 02:08:28 by modiepge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void heredoc_expand(int fd, char *line)
-{
-	char *lline;
+// t_file	*heredoc_expand(t_token *token)
+// {
+// 	char *lline;
 
-	lline = line;
-	while (lline[0])
-	{
-		write(fd, lline, lline - ft_strchr(lline, '$'));
-		lline = ft_strchr(lline, '$') + 1;
-	}
-}
-
-void	heredoc_in(t_expression *atom, t_token **token)
-{
-	char	*line;
-	char	*delimiter;
-	bool	expand;
-
-	(void)atom;
-	if ((*token)->next && (*token)->next->type == TOK_WORD)
-	{
-		delimiter = (*token)->next->content;
-		expand = !((*token)->next->is_quoted);
-	}
-	line = readline("> ");
-	gc_mode(GC_WORKING);
-	while (line)
-	{
-		if (line && !ft_strncmp(delimiter, line, ft_strlen(line)))
-		{
-			free(line);
-			break ;
-		}
-		gc_strjoin((*token)->next->content, line);
-		gc_strjoin((*token)->next->content, "\n");
-		free(line);
-		line = readline("> ");
-	}
-	gc_mode(GC_TEMPORARY);
-	(*token)->next->content = gc_strdup((*token)->next->content);
-	*token = (*token)->next;
-}
+// 	lline = line;
+// 	while (lline[0])
+// 	{
+// 		write(fd, lline, lline - ft_strchr(lline, '$'));
+// 		lline = ft_strchr(lline, '$') + 1;
+// 	}
+// }
 
 t_file	*redirect_out(t_expression *atom, t_token **token)
 {
@@ -95,11 +64,11 @@ t_filetype	get_redirect(t_expression *atom, t_token **token)
 	file = NULL;
 	if ((*token)->type == TOK_LESS)
 		file = redirect_in(atom, token);
-	else if ((*token)->type == TOK_GREATER 
+	else if ((*token)->type == TOK_GREATER
 		|| (*token)->type == TOK_DOUBLE_GREATER)
 		file = redirect_out(atom, token);
 	// else if ((*token)->type == TOK_DOUBLE_LESS)
-	// 	file = heredoc_in(atom, token);
+	// 	file = heredoc_expand(token);
 	if (file)
 		return (file->type);
 	return (-1);
