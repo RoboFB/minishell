3ce2a2f455ef18731468e44a37a17b0b17d51e68
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-10-17 16:45:01 +0200

    feat: built in for command with no name

---
+++ src/execution/built_ins/blt_noname.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   blt_noname.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 16:27:01 by rgohrig           #+#    #+#             */
+/*   Updated: 2025/10/17 16:29:39 by rgohrig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void blt_noname(t_expression *command)
+{
+	if (command->name == NULL)
+		set_exit_code(EXIT_OK);
+	else
+	{
+		msg_error("noname command", "bad internal call error");
+		set_exit_code(EXIT_GENERAL_ERROR);
+	}
+	return ;
+}
