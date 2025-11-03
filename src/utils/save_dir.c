f23e501b21476ca7f5f138a0a7a4c161ba768c11
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-11-03 16:54:04 +0100

    feat: wrapper for opendir

--- src/utils/save_dir.c
+++ src/utils/save_dir.c
@@ -6,7 +6,7 @@
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 20:47:03 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/10/31 17:52:42 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/10/31 18:23:23 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,20 @@ char	*save_getcwd(char *buf, size_t size)
 	path = getcwd(buf, size);
 	if (path == NULL)
 		perror_exit("getcwd failed", EXIT_GENERAL_ERROR);
-			// maby not exit here if i dont have permission
 	return (path);
 }
 
-void	save_chdir(char *new_dir)
-{
-	if (chdir(new_dir) == -1)
-		perror_msg_exit("chdir failed", new_dir, EXIT_GENERAL_ERROR);
-	return ;
-}
-
+// only use for wildcards
 DIR	*save_opendir(char *path)
 {
-	DIR *dir_stream;
+	DIR	*dir_stream;
 
 	dir_stream = opendir(path);
 	if (dir_stream == NULL)
-		perror_msg_exit("opendir", path, EXIT_GENERAL_ERROR);
+	{
+		perror(path);
+		set_exit_code(EXIT_GENERAL_ERROR);
+		*interrupted() = true;
+	}
 	return (dir_stream);
 }
