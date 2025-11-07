b5d26f4936e96c1741cf0e62c2cd00540780f478
modiepge <modiepge@student.42heilbronn.de>
2025-11-07 13:41:41 +0100

    fix: multi-space line no longer syntax errors

--- src/parsing/parse.c
+++ src/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: modiepge <modiepge@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 16:40:33 by modiepge          #+#    #+#             */
-/*   Updated: 2025/11/04 19:10:12 by modiepge         ###   ########.fr       */
+/*   Updated: 2025/11/07 13:40:31 by modiepge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 void	parse(char *line, t_tokens *list)
 {
 	tokenize(line, list);
+	if (!valid_collection(list))
+		return ;
 	quote(list);
 	strip_quotes(list);
 	set_delimiters(list);
